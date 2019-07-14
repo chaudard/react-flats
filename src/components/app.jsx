be9888195acfe093ff8736f5a25b11529e331832
MICHEL Dany <donnetonfalzard+gitlab@gmail.com>
2019-07-14 14:50:25 +0200

    add main app component

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <p>My app rendering</p>
+        )
+    }
+}
+export default App;
